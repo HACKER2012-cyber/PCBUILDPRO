811f037ff04fa958f9bd43a4de138a9544a74740
HACKER2012-cyber <nikolaydelchev2012@gmail.com>
2025-10-24 20:24:33 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,2 @@
+alert("Добре дошъл в моят първи сайт!");
+alert("Направете ми теста за PC! URL:https://docs.google.com/forms/d/e/1FAIpQLSdpLNSH1GwhGVGtJW8GLHyVhuIbcQwTTHT1S1-9I6B7VkI5Vw/viewform?usp=dialog")
